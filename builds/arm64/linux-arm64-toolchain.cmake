300e0ae9b276d5b9647867f1beeb5e8b60c2f231
ssbeatty <wang918562230@qq.com>
2025-01-06 15:46:48 +0800

    feat: 增加arm64的lib文件

---
+++ builds/arm64/linux-arm64-toolchain.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_FLAGS "-march=armv8-a")
+set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
---
+++ iec61850/config_armv8.go
@@ -0,0 +1,7 @@
+//go:build linux && arm64
+
+package iec61850
+
+// #cgo CFLAGS: -I./inc/hal/inc -I./inc/common/inc -I./inc/goose -I./inc/iec61850/inc -I./inc/iec61850/inc_private -I./inc/logging -I./inc/mms/inc -I./inc/mms/inc_private -I./inc/mms/iso_mms/asn1c
+// #cgo LDFLAGS: -static-libgcc -static-libstdc++ -L./lib/linux_armv8 -liec61850 -lhal
+import "C"
